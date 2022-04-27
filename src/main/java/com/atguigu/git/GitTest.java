0e8c48e52be126cb676c15b495ade8e6812e6fa7
atguigulijia <104440543+atguigulijia@users.noreply.github.com>
2022-04-27 17:03:58 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("thanke you");
         System.out.println("puch test");
+        System.out.println("pull test");
     }
 }
