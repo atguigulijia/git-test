8e8194a2d0379c384a1e89186c471ca5bb0bd9fe
lijia <lijia@qq.com>
2022-04-27 15:42:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lijia
+ * @create 2022-04-27 15:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
